6c18b6b7207960ae3aa5d4c95714149c397ff0dc
LogicalEffort <pamark126@gmail.com>
2026-09-20 23:37:50 -0400

    Player movement and grouping

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ import pygame
 
 
 def main():
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt: float = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
+
+        #limits FPS to 60
         dt = clock.tick(60)/1000
 
 
